4d378456f2ce2bcec4104845667ee4ecc89f021f
Alexandros Christopoulos <alex1951132@gmail.com>
2024-05-10 17:19:52 +0300

    cleanup template

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,3 @@
-import joi from 'joi';
-
 export const config = {
   SERVER_PORT: process.env.SERVER_PORT || 8080,
 };
-
-const result = joi
-  .object({
-    SERVER_PORT: joi.number().integer(),
-  })
-  .validate(config);
-
-if (result.error) {
-  console.log(result.error.details);
-  process.exit(1);
-}
